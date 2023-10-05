2e4070fd680755cfbe818719276927692fc942b1
Rufus <likeness14@gmail.com>
2023-10-05 11:15:13 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main- This is the main fuction of my code
+ * Return: Always return zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
