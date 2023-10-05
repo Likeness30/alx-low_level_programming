0e3dee07ad4e484b09c7b6203e6f9d932dbc6f90
Rufus <likeness14@gmail.com>
2023-10-05 11:03:06 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main- This is the main function of my code
+ * Return: Always return zero
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
