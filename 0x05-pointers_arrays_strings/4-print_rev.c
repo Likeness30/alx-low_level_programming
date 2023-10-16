c6e6ac16ebca5b40778cf775ce54a718b6937dc5
Rufus <likeness14@gmail.com>
2023-10-16 16:26:47 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  print_rev - The function name
+ *  @s: Funtion parameter
+ */
+
+void print_rev(char *s)
+{
+	int a;
+	int count = 0;
+
+for (a = 0; s[a] != '\0'; a++)
+	count++;
+for (a = count - 1; a >= 0; a--)
+	_putchar(s[a]);
+_putchar('\n');
+}
