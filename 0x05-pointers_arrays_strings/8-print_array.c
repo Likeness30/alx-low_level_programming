38d654ba9f0986b50d6b66b4d6958b89764cf1c6
Rufus <likeness14@gmail.com>
2023-10-16 18:58:24 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - Function name
+ * @a: 1st parameter
+ * @n: 2nd parameter
+ */
+
+void print_array(int *a, int n)
+{
+int b;
+
+for (b = 0; b < n; b++)
+{
+printf("%d", b[a]);
+{
+	if (b < n - 1)
+		printf(", ");
+}
+	printf("\n");
+}
+}
