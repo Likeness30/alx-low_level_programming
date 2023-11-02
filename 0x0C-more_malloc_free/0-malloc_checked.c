f6ce2bd84a10dd8e3ff5126d79b2aa400002432f
Rufus <likeness14@gmail.com>
2023-11-02 11:12:00 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * malloc_checked - This function allocates memory
+ * @b: The parameter declared
+ * Return: A pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *result;
+
+	result = malloc(b);
+	if (result == NULL)
+exit(98);
+	return (result);
+}
