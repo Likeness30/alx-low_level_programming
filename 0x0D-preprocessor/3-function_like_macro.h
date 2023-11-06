3193b7f089d5a5293175ebdfc8205b99e7201522
Rufus <likeness14@gmail.com>
2023-11-06 04:38:17 -0800

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef LIKE_MACRO_H
+#define LIKE_MACRO_H
+
+/**
+ * File - 3-function_like_macro
+ * @x: The input parameter
+ * Return: 0 (success)
+ */
+
+#define ABS(x) ((x) < 0 ? (-1) * (x) : (x))
+
+#endif
