aefcc1bfda7a595f147954014e8213f767cb48ae
Rufus <likeness14@gmail.com>
2023-11-09 06:32:53 -0800

    0-sum_them_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,7 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNTIONS_H
-#include <stdlib.h>
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdarg.h>
 #include <string.h>
-
-int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
 
 #endif
