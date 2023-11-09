311232b65607452918cb7ee3354c9b4e531d2d09
Rufus <likeness14@gmail.com>
2023-11-09 12:24:59 -0800

    Updated 2-prints_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(ptr_str, char *);
 
-		if (separator == NULL)
+		if (str == NULL)
 		{
 			printf("(nil)");
 		}
