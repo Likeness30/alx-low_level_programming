57c902b1f9c4caf4f4482d4f41c3dde7109c2792
Likeness30 <likeness14@gmail.com>
2023-11-20 22:58:44 -0800

    4-free_listint.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,13 +10,17 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
-	listint_t *temp;
+	listint_t *tmp;
 
+	new = malloc(sizeof(listint_t));
 	if (head == NULL)
 	{
 		return (NULL);
 	}
-	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 	new->n = n;
 	new->next = NULL;
 	if (*head == NULL)
@@ -24,12 +28,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = new;
 		return (new);
 	}
-	temp = *head;
-	while (temp->next != NULL)
+	tmp = *head;
+	while (tmp->next != NULL)
 	{
-		temp = temp->next;
+		tmp = tmp->next;
+		tmp->next = new;
 	}
-	temp->next = new;
 	return (new);
-
 }
