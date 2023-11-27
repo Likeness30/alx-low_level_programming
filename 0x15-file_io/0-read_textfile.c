72dcfa4e6fdb3df1263708793021b2c384387d44
Likeness30 <likeness14@gmail.com>
2023-11-27 09:58:32 -0800

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile -Reads a text file and prints it to the POSIX standard output
+ * @filename: The name of the file
+ * @letters: The letters to readand  print
+ * Return: NULL or 0.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters);
+{
+	int f;
+	ssize_t abc, xyz;
+	char *buffer;
+
+	if (!filename)
+	{
+		return (0);
+	}
+	f = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
+	{
+		return (0);
+	}
+	abc = read(f, buffer, letters);
+	xyz = write(STDOUT_FILENO, buffer, xyz);
+	close(f);
+	free(buffer);
+	return (xyz);
+}
